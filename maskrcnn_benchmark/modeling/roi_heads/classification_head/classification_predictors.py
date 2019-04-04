7266eddd1a2fce7dc9f8b6f8b5e4b129ef7e14a8
unknown <pg385507@students.mimuw.edu.pl>
2019-04-04 07:25:05 +0100

    Classification_Head introduction

---
+++ maskrcnn_benchmark/modeling/roi_heads/classification_head/classification_predictors.py
@@ -0,0 +1,34 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+from maskrcnn_benchmark.modeling import registry
+from torch import nn
+
+
+@registry.CLASSIFICATION_PREDICTOR.register("FPNPredictor")
+class FPNPredictor(nn.Module):
+    def __init__(self, cfg, in_channels):
+        super(FPNPredictor, self).__init__()
+        num_classes = cfg.MODEL.CLASSIFICATION_HEAD.NUM_CLASSES
+        representation_size = in_channels
+
+        self.cls_score = nn.Linear(representation_size, num_classes)
+        num_bbox_reg_classes = 2 if cfg.MODEL.CLS_AGNOSTIC_BBOX_REG else num_classes
+        self.bbox_pred = nn.Linear(representation_size, num_bbox_reg_classes * 4)
+
+        nn.init.normal_(self.cls_score.weight, std=0.01)
+        nn.init.normal_(self.bbox_pred.weight, std=0.001)
+        for l in [self.cls_score, self.bbox_pred]:
+            nn.init.constant_(l.bias, 0)
+
+    def forward(self, x):
+        if x.ndimension() == 4:
+            assert list(x.shape[2:]) == [1, 1]
+            x = x.view(x.size(0), -1)
+        scores = self.cls_score(x)
+        bbox_deltas = self.bbox_pred(x)
+
+        return scores, bbox_deltas
+
+
+def make_classification_predictor(cfg, in_channels):
+    func = registry.CLASSIFICATION_PREDICTOR[cfg.MODEL.CLASSIFICATION_HEAD.PREDICTOR]
+    return func(cfg, in_channels)
