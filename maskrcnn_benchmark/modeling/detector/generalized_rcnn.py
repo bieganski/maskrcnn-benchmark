dab2aa6a0df61ab38245c6db5069faa1e5c99d64
Mateusz <bieganski.m@wp.pl>
2019-05-06 23:30:36 +0200

    test

--- maskrcnn_benchmark/modeling/detector/generalized_rcnn.py
+++ maskrcnn_benchmark/modeling/detector/generalized_rcnn.py
@@ -47,6 +47,8 @@ class GeneralizedRCNN(nn.Module):
             raise ValueError("In training mode, targets should be passed")
         images = to_image_list(images)
         features = self.backbone(images.tensors)
+        print(features.size())
+        exit(1)
         proposals, proposal_losses = self.rpn(images, features, targets)
         if self.roi_heads:
             x, result, detector_losses = self.roi_heads(features, proposals, targets)
