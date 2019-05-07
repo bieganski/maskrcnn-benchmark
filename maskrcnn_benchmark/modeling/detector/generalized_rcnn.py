862bef22d6d63f971f610f3949bcea824ec0171b
Mateusz <bieganski.m@wp.pl>
2019-05-07 10:52:53 +0200

    test

--- maskrcnn_benchmark/modeling/detector/generalized_rcnn.py
+++ maskrcnn_benchmark/modeling/detector/generalized_rcnn.py
@@ -47,6 +47,7 @@ class GeneralizedRCNN(nn.Module):
             raise ValueError("In training mode, targets should be passed")
         images = to_image_list(images)
         features = self.backbone(images.tensors)
+        print(images)
         # type(features) = tuple
         # print(features)
         # C4 extractor:
@@ -64,6 +65,7 @@ class GeneralizedRCNN(nn.Module):
         #   torch.Size([2, 256, 3, 2])
         for el in features:
             print(el.size())
+        print("------------------")
         proposals, proposal_losses = self.rpn(images, features, targets)
         if self.roi_heads:
             x, result, detector_losses = self.roi_heads(features, proposals, targets)
