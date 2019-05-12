7d0329f6cd7d5b8afef2a1b69b5a4a486207d60b
Hoxmot <hoxmot@gmail.com>
2019-05-12 22:04:58 +0200

    [sort_images.sh] - a few typos

--- sort_images.sh
+++ sort_images.sh
@@ -9,6 +9,7 @@ IMAGES_PATH=${VOC_PATH}/JPEGImages
 if [ -f "pascal/train_list" ] ; then
     if [ -d "${TRAIN_PATH}" ] ; then
         rm -rf ${TRAIN_PATH}
+    fi
     mkdir ${TRAIN_PATH}
     while read $f ; do
         cp ${IMAGES_PATH}/${f} ${TRAIN_PATH}/${f}
@@ -20,6 +21,7 @@ fi
 # if [ -f "pascal/test_list" ] ; then
 #     if [ -d "${test_PATH}" ] ; then
 #         rm -rf ${test_PATH}
+#     fi
 #     mkdir ${test_PATH}
 #     while read $f ; do
 #         cp ${IMAGES_PATH}/${f} ${test_PATH}/${f}
@@ -31,6 +33,7 @@ fi
 if [ -f "pascal/val_list" ] ; then
     if [ -d "${val_PATH}" ] ; then
         rm -rf ${val_PATH}
+    fi
     mkdir ${val_PATH}
     while read $f ; do
         cp ${IMAGES_PATH}/${f} ${val_PATH}/${f}
