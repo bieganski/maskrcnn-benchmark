493b5bca25b5f559f921df66d0b1fbd17408a01e
zppmt <zppmt@bozena>
2019-05-22 21:15:23 +0200

    proper minival

--- zrob_minival.py
+++ zrob_minival.py
@@ -8,13 +8,22 @@ ZOSTAW_JEDEN_NA = 25
 
 i = 0
 nowe_zdjecia = []
+zdjecia = set()
 for img in a['images']:
 	if i % ZOSTAW_JEDEN_NA == 0:
+		zdjecia.add(img['id'])
 		nowe_zdjecia.append(img)
 	i += 1
 
 a['images'] = nowe_zdjecia
 
+nowe_anno = []
+
+for anno in a['annotations']:
+	if anno['image_id'] in zdjecia:
+		nowe_anno.append(anno)
+
+a['annotations'] = nowe_anno
 
 with open('./pascal/coco_detail_minival.json', 'w') as outfile:
 	json.dump(a, outfile, sort_keys=True, indent=2)
