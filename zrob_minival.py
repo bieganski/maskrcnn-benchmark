5fc772e63b8e20f8ea51d9017d0727b3ff3d5da3
zppmt <zppmt@bozena>
2019-05-22 20:37:29 +0200

    make minival script added

---
+++ zrob_minival.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env	python3
+
+import json
+
+a = json.load(open('detail_val.json', 'rb'))
+
+ZOSTAW_JEDEN_NA = 10
+
+i = 0
+nowe_zdjecia = []
+for img in a['images']:
+	if i % ZOSTAW_JEDEN_NA == 0:
+		nowe_zdjecia.append(img)
+	i += 1
+
+a['images'] = nowe_zdjecia
+
+
+with open('./pascal/minival.json', 'w') as outfile:
+	json.dump(a, outfile, sort_keys=True, indent=2)
+
+
