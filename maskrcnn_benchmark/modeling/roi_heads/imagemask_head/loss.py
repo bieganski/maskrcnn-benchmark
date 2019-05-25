67d1261edb760b20ad455f983e19a269ed88f931
Mateusz <bieganski.m@wp.pl>
2019-05-25 23:56:57 +0200

    test

--- maskrcnn_benchmark/modeling/roi_heads/imagemask_head/loss.py
+++ maskrcnn_benchmark/modeling/roi_heads/imagemask_head/loss.py
@@ -11,11 +11,12 @@ class SegmentationMaskLoss(object):
 
     def __call__(self, x, gt):
         # gt - lista targetów
-        assert False, (x.size(), gt[0].size())
+        # assert False, (x.size(), gt[0].size())
+        # AssertionError: (torch.Size([1, 60, 426, 500]), torch.Size([426, 500]))
         assert x.size()[0] == 1
         x = x.squeeze(0)
         assert x.size()[0] == self.num_cls
-        gt = torch.Tensor(gt) # in case of being np.ndarray
+        gt = gt[0] # TODO wieksze batche
         assert gt.size() == x.size()[-2:]
         dim_ok_gt = torch.zeros_like(x, requires_grad=True)
 
@@ -23,7 +24,7 @@ class SegmentationMaskLoss(object):
         for i in range(gt.size(0) + 1):
             for j in range(gt.size(1) + 1):
                 cls = gt[i, j]
-                dim_ok_gt[i, j, cls] = 1
+                dim_ok_gt[cls, i, j] = 1
 
         assert dim_ok_gt.sum() == torch.nonzero(gt).sum()
 
