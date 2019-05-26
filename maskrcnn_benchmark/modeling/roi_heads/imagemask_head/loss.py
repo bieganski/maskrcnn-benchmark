2961d782161f4ff9ca7fcd39decf846f840ce239
Mateusz <bieganski.m@wp.pl>
2019-05-26 23:42:04 +0200

    test

--- maskrcnn_benchmark/modeling/roi_heads/imagemask_head/loss.py
+++ maskrcnn_benchmark/modeling/roi_heads/imagemask_head/loss.py
@@ -24,7 +24,7 @@ class SegmentationMaskLoss(object):
         # thats lame, but should works
         for i in range(gt.size(0) + 1):
             for j in range(gt.size(1) + 1):
-                cls = gt[i, j]
+                cls = gt[i, j].item()
                 dim_ok_gt[cls, i, j] = 1
 
         assert dim_ok_gt.sum() == torch.nonzero(gt).sum()
