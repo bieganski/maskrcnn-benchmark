36a3f6e515119a2268f9bcd493bf98b8762a8153
Mateusz <bieganski.m@wp.pl>
2019-05-27 23:31:56 +0200

    test

--- maskrcnn_benchmark/engine/inference.py
+++ maskrcnn_benchmark/engine/inference.py
@@ -22,7 +22,7 @@ def compute_on_dataset(model, data_loader, device):
         images = images.to(device)
         with torch.no_grad():
             output = model(images)
-            assert False, (output.shape, output.sum())
+            # assert False, (output.shape, output.sum())
             output = [o.to(cpu_device) for o in output]
         results_dict.update(
             {img_id: result for img_id, result in zip(image_ids, output)}
