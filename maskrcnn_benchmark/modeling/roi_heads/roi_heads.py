a1bf6e7bab662a117796fe5c0a096b9736c53d7d
Mateusz <bieganski.m@wp.pl>
2019-05-27 11:19:20 +0200

    test

--- maskrcnn_benchmark/modeling/roi_heads/roi_heads.py
+++ maskrcnn_benchmark/modeling/roi_heads/roi_heads.py
@@ -206,7 +206,7 @@ class CombinedROIHeads(torch.nn.ModuleDict):
                 h = max(h, shape[1])
             return (c, w, h)
 
-        new_shape = including_rectangle(cfg.ROI_IMAGEMASK_HEAD.NUM_CLASSES, shapes)
+        new_shape = including_rectangle(self.cfg.ROI_IMAGEMASK_HEAD.NUM_CLASSES, shapes)
 
         def _pad(shape, array, padval = -1):
             padded = np.full(tuple(shape), padval)
