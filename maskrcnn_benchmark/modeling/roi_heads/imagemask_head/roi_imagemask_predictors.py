352ca7d64b71986f3f3ede9eb5fa8a9315a395c7
Mateusz <bieganski.m@wp.pl>
2019-05-27 12:06:38 +0200

    test

--- maskrcnn_benchmark/modeling/roi_heads/imagemask_head/roi_imagemask_predictors.py
+++ maskrcnn_benchmark/modeling/roi_heads/imagemask_head/roi_imagemask_predictors.py
@@ -29,6 +29,7 @@ class MaskRCNNImageMakPredictor(nn.Module):
                 h = max(h, shape[-1])
             return [w, h]
 
+        assert False, x
         x = x[0] # output z FPNa największej rozdzielczości
 
         new_shape = [x[0], x[1]] + including_rectangle(img_sizes) # batch size, num_features, w, h
