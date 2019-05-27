6707ac300c36f2b7614697ddb19894ac881d556c
Mateusz <bieganski.m@wp.pl>
2019-05-27 12:42:24 +0200

    test

--- maskrcnn_benchmark/modeling/roi_heads/imagemask_head/loss.py
+++ maskrcnn_benchmark/modeling/roi_heads/imagemask_head/loss.py
@@ -8,15 +8,17 @@ class SegmentationMaskLoss(object):
         self.num_cls = num_cls
         self.soft = F.softmax
         self.loss = F.binary_cross_entropy
+        self.loss2 = F.binary_cross_entropy_with_logits
 
     def __call__(self, x, gt):
         assert gt.shape == x.shape
 
         # TODO tu moze zle dzialac przez mismatche obrazkow, zrobic wtedy dla kazdej warstwy pojedynczo
-        x = self.soft(x, dim=1)
-        loss = self.loss(x, gt)
-        print("<<<<<<<<<<<<LOSS:")
-        print(loss)
+        # x = self.soft(x, dim=1)
+        # loss = self.loss(x, gt)
+        # print("<<<<<<<<<<<<LOSS:")
+        # print(loss)
+        loss = self.loss2(x, gt)
         return loss
 
 
