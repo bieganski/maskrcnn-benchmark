7913e5c2d6e530bb011c93767ee85197ff3463c2
Mateusz <bieganski.m@wp.pl>
2019-05-28 11:58:37 +0200

    test

--- tools/test_net.py
+++ tools/test_net.py
@@ -65,7 +65,10 @@ def main():
     checkpointer = DetectronCheckpointer(cfg, model, save_dir=output_dir)
     _ = checkpointer.load(cfg.MODEL.WEIGHT)
 
-    iou_types = ("bbox",)
+    if cfg.MODEL.MASK_ON or cfg.MODEL.KEYPOINT_ON:
+        iou_types = ("bbox",)
+    else:
+        iou_types = ()
     if cfg.MODEL.MASK_ON:
         iou_types = iou_types + ("segm",)
     if cfg.MODEL.KEYPOINT_ON:
