ad6ea2da12fd53339fd21c2d4b0ce21225986ffd
Mateusz <bieganski.m@wp.pl>
2019-05-28 12:01:49 +0200

    test

--- maskrcnn_benchmark/data/datasets/evaluation/coco/coco_eval.py
+++ maskrcnn_benchmark/data/datasets/evaluation/coco/coco_eval.py
@@ -39,6 +39,7 @@ def do_coco_evaluation(
         return
     logger.info("Preparing results for COCO format")
     coco_results = {}
+    assert False, iou_types
     if "bbox" in iou_types:
         logger.info("Preparing bbox results")
         coco_results["bbox"] = prepare_for_coco_detection(predictions, dataset)
