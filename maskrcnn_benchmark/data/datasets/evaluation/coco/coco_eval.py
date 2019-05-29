1010d5d13ae6c38d3a241a15312dea628eee554d
Mateusz <bieganski.m@wp.pl>
2019-05-29 12:16:51 +0200

    test

--- maskrcnn_benchmark/data/datasets/evaluation/coco/coco_eval.py
+++ maskrcnn_benchmark/data/datasets/evaluation/coco/coco_eval.py
@@ -52,11 +52,8 @@ def do_coco_evaluation(
         coco_results['semantic'] = prepare_for_semantic(predictions, dataset)
         for el in zip(predictions, dataset):
             from maskrcnn_benchmark.structures.bounding_box import BoxList
-            torch.Tensor.__str__ = lambda self: str(self.shape)
             torch.Tensor.__repr__= lambda self: str(self.shape)
-            assert False, el
-            print(el[0].shape)
-            bl = el[1][1]
+            bl = el[2]
             # def pp(obj):
             #     if isinstance(obj, list) or isinstance(obj, tuple):
 
