5b56a7052420e65b026ee85251d2ea63c9025bbe
Mateusz <bieganski.m@wp.pl>
2019-05-29 12:25:16 +0200

    test

--- maskrcnn_benchmark/data/datasets/evaluation/coco/coco_eval.py
+++ maskrcnn_benchmark/data/datasets/evaluation/coco/coco_eval.py
@@ -50,11 +50,11 @@ def do_coco_evaluation(
         coco_results['keypoints'] = prepare_for_coco_keypoint(predictions, dataset)
     if 'semantic' in iou_types:
         coco_results['semantic'] = prepare_for_semantic(predictions, dataset)
-        for el in zip(predictions, dataset):
+        for el in zip(coco_results['semantic'], dataset):
             from maskrcnn_benchmark.structures.bounding_box import BoxList
             torch.Tensor.__repr__= lambda self: str(self.shape)
 
-            # assert False, el
+            assert False, el
             # AssertionError: (torch.Size([200, 272]),
             #                 (torch.Size([3, 800, 1066]),
             #                       BoxList(num_boxes=2,
@@ -69,6 +69,7 @@ def do_coco_evaluation(
             mask = getCWHMulticlassMask(bl)
             print(type(mask))
             print(mask.shape)
+            img =
             exit(1)
 
     results = COCOResults(*iou_types)
